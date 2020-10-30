1056414f98377cc1367d7aa67d08e0de2681c1e3
JESUS AUGUSTO ANGLAS AYME <jesusa2624@gmail.com>
2020-10-30 16:25:49 -0500

    Hasta el cap 4

---
+++ condicionales.py
@@ -0,0 +1,16 @@
+# edad = int(input("escribe tu edad: "))
+# if edad > 17:
+#     print("Eres mayor de edad")
+# else:
+#     print("eres menos de edad")
+
+numero = int(input("escribe un numero"))
+
+if numero > 5:
+    print("es mayor a 5")
+elif numero ==5:
+    print ("es igual a 5")
+else:
+    print("Es menor a 5")
+
+
--- conversor.py
+++ conversor.py
@@ -1,10 +1,34 @@
 # Tarea del curso Platzi en moneda peruana
-soles = input("¿Cuantos soles tienes?: ")
-soles = float(soles)
-valor_dolar = 3.40
-dolares = soles / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dolares")
+def conversor(tipo_pesos,valor_dolar):
+    pesos = input("¿Cuantos pesos" + tipo_pesos + "tienes?:" )
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+
+
+menu = """
+Bienvenido al converso de monedas😊
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Eliga una opcion: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    conversor("colombianos",3875)
+elif opcion == 2:
+    conversor("argentinos",65)
+elif opcion == 3:
+    conversor("mexicanos",24)
+else:
+    print('ingresa una opcion correcta por favor')
+    
+
+
 
       
